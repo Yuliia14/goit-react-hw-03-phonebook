f25edc835d15085716db8ecb0d06eb7461083036
Yuliia Bevko <bevko.yulka@gmail.com>
2021-01-30 23:11:03 +0200

    dfsdfsd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import Filter from './components/Filter/Filter';
 
 export default class App extends Component {
   state = {
-    contacts: [ ],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
     filter: '',
   }
   addContact = ({name, number}) => {
@@ -39,14 +44,13 @@ export default class App extends Component {
   }
 
   getVisibleContacts = () => {
-    
     const { contacts, filter } = this.state;
-    if(contacts.length > 0) {
-      return contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter.toLowerCase()),
-     )
+    
+    return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()))
+  
     }
-  }
+
   componentDidMount() {
     const contacts = localStorage.getItem('contacts')
     const parsedContacts = JSON.parse(contacts)
